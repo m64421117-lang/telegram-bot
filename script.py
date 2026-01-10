880e14d7bfbb06cf12f588d42786bc888d99567a
Kareem-Khaled <m64421117@gmail.com>
2026-01-10 22:09:53 +0200

    Update Referer header for Sakani API request

--- script.py
+++ script.py
@@ -52,7 +52,7 @@ try:
         "Accept": "application/json",
         "Content-Type": "application/json",
         "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
-        "Referer": "https://sakani.sa/app/marketplace",
+        "Referer": "https://sakani.sa/app/marketplace?marketplace_purpose=buy&product_types=lands&target_segment_info=beneficiary&land_type=all&sort=-publish_date",
         "Platform": "web",
         "Visitor-Id": "29b7f97eb471f58649fb45a0ed260d96"
     }
